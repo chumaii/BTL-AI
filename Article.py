792f36f7c27596f191608d5e20b9be27a5a17e0b
m3in3 <103488548+chumaii@users.noreply.github.com>
2023-05-08 15:51:28 +0700

    Update Article.py

--- Article.py
+++ Article.py
@@ -3,4 +3,4 @@ class Article:
         self.link = link
         self.description = description
 # Đây là một định nghĩa class cho một đối tượng bài báo,
-# với các thuộc tính bao gồm tiêu đề (title), đường dẫn tới bài báo (link), và mô tả (description).
+# với các thuộc tính bao gồm đường dẫn tới bài báo (link), và mô tả (description).
